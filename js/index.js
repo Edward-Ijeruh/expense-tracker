f297f790ce82412a402a1e3d274d622dc2e1da59
Edward Ijeruh <zacharaiah17@gmail.com>
2022-08-18 16:34:40 +0100

    Local storage

--- js/index.js
+++ js/index.js
@@ -15,21 +15,55 @@ let incomeVal = income.value
 let expenseVal = expense.value
 let current = new Date();
 let cDate = current.getFullYear() + '-' + (current.getMonth() + 1) + '-' + current.getDate();
-let cTime = current.getHours() + ":" + current.getMinutes() + ":" + current.getSeconds();
+let cTime = current.getHours() + ":" + current.getMinutes();
 let dateTime = cDate + ' ' + cTime;
+let img = `<img src="../images/plus.png">`;
+let imgTwo = `<img src = "../images/remove.png">`;
 
 
+//FLOAT BUTTON
 
 floatBtn.addEventListener('click', () => {
     main.classList.toggle('d-none');
     main.classList.toggle('active'); 
 })
 
+//EXIT BUTTON
+
 exitBtn.addEventListener("click", function() {
     main.classList.toggle('active');
     main.classList.toggle('d-none');
 })
 
+//LOCAL STORAGE FXNS
+
+const userData = [];
+const userExpenseData = [];
+
+function addIncome(balanceAmt, incomeAmt, incomeTime) {
+    userData.push({
+        Current_Balance: balanceAmt,
+        Stated_Income: incomeAmt,
+        Stated_Income_Time: incomeTime
+    })
+    localStorage.setItem("userData", JSON.stringify(userData))
+
+    return {balanceAmt, incomeAmt, incomeTime}
+} 
+
+function addExpense(balanceAmt, expenseAmt, expenseTime) {
+    userExpenseData.push({
+        Current_Balance: balanceAmt,
+        Stated_Expense: expenseAmt,
+        Stated_Expense_Time: expenseTime
+    })
+    localStorage.setItem("userExpenseData", JSON.stringify(userExpenseData))
+
+    return {balanceAmt, expenseAmt, expenseTime}
+} 
+
+
+// INCOME BUTTON FUNCTION
 
 incomeBtn.addEventListener("click", function () {
     if (income.value === "") {
@@ -42,19 +76,29 @@ incomeBtn.addEventListener("click", function () {
         history.classList.add("history")
         let listItem = document.createElement("div")
         let listItemTwo = document.createElement("div")
-        listItem.style.color = "green";
-        listItemTwo.style.color = "green";
+        listItem.style.color = "black";
+        listItemTwo.style.color = "black";
         emptyHistory.classList.add("d-none")
         historyContainer.append(history)
         history.append(listItem)
         history.append(listItemTwo)
-        listItem.textContent = `Income: ₦${incomeVal}`
+        listItem.innerHTML = `${img} ₦${incomeVal}`
         listItemTwo.textContent = `${dateTime}`
-        income.value = ""; 
+        income.value = "";
+        addIncome( balance.innerText, listItem.innerHTML, listItemTwo.textContent);
+        // let userData = {
+        //     balanceAmt: balance.innerText,
+        //     incomeAmt: listItem.innerHTML,
+        //     incomeTime: listItemTwo.textContent
+        // };
+        // userData = JSON.stringify(userData)
+        // localStorage.setItem("localst",userData)
         const toast = new bootstrap.Toast(toastIncome)
         toast.show()
     }
 })
+
+//EXPENSE BUTTON FUNCTION
 
 expenseBtn.addEventListener("click", function (){
     if (expense.value === "") {
@@ -67,15 +111,23 @@ expenseBtn.addEventListener("click", function (){
         history.classList.add("history")
         let listItem = document.createElement("div")
         let listItemTwo = document.createElement("div")
-        listItem.style.color = "red";
-        listItemTwo.style.color = "red";
+        listItem.style.color = "black";
+        listItemTwo.style.color = "black";
         emptyHistory.classList.add("d-none")
         historyContainer.append(history)
         history.append(listItem)
         history.append(listItemTwo)
-        listItem.textContent = `Income: ₦${expenseVal}`
+        listItem.innerHTML = `${imgTwo} ₦${expenseVal}`
         listItemTwo.textContent = `${dateTime}`
         expense.value = "";
+        addExpense( balance.innerText, listItem.innerHTML, listItemTwo.textContent);
+        // let userDataTwo = {
+        //     balanceAmt: balance.innerText,
+        //     expenseAmt: listItem.innerHTML,
+        //     expenseTime: listItemTwo.textContent
+        // };
+        // userDataTwo = JSON.stringify(userDataTwo)
+        // localStorage.setItem("localstTwo",userDataTwo)
         const toast = new bootstrap.Toast(toastExpense)
         toast.show()
     }
